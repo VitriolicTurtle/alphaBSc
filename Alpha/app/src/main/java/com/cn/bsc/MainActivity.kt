80fda44ae9b1051109e5dbde2902d81e061e45ac
jesperu7 <jesperu7@hotmail.com>
2021-01-08 20:29:32 +0100

    initial commit with basic db login functuinality

--- Alpha/app/src/main/java/com/cn/bsc/MainActivity.kt
+++ Alpha/app/src/main/java/com/cn/bsc/MainActivity.kt
@@ -2,10 +2,67 @@ package com.cn.bsc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var mAuth: FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        onStart()
+        val user:String = "jesperu7@hotmail.com"
+        val password:String = "123456"
+        //createUser(user, password)
+        login(user, password)
     }
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = mAuth!!.currentUser  // current user
+
+        // current user ID
+        val userID = FirebaseAuth.getInstance().currentUser!!.uid
+
+    }
+
+    private fun createUser(email: String, password: String) {
+        mAuth!!.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Toast.makeText(
+                        this@MainActivity,
+                        "User successfully created!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(this@MainActivity, "Creating user failed", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun login(email: String, password: String) {
+        mAuth!!.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(
+                this
+            ) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Toast.makeText(this@MainActivity, "Logged in!", Toast.LENGTH_SHORT).show()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(this@MainActivity, "Login failed", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
 }
