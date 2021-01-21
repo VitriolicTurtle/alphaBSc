44a8af91f6ab99629cec85e2ecfdae5368d23628
jesperu7 <jesperu7@hotmail.com>
2021-01-21 15:44:27 +0100

    login and register fragments working

--- Alpha/app/src/main/java/com/cn/bsc/MainActivity.kt
+++ Alpha/app/src/main/java/com/cn/bsc/MainActivity.kt
@@ -4,84 +4,47 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.cn.bsc.fragments.LoginFragment
+import com.cn.bsc.fragments.MainFragment
+import com.cn.bsc.fragments.RegisterFragment
 import com.google.android.gms.tasks.OnFailureListener
 import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-    var DBTest = DBTest()
-    val db = FirebaseFirestore.getInstance()
-    var mAuth: FirebaseAuth? = null
+
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        onStart()
 
-        //createUser(user, password)
-        login(DBTest.userEmail, DBTest.password)
-
-        // current user ID
-        val userID = FirebaseAuth.getInstance().currentUser!!.uid
-
-        // Create a new user with a first and last name
-        val user = hashMapOf(
-        "userid" to userID,
-        "name" to DBTest.userName,
-        "score" to DBTest.userScore
-        )
-
-        db.collection("users").document(userID)
-                .set(user)
-                .addOnSuccessListener { Log.d("Successfully added to DB", "DocumentSnapshot successfully written!") }
-                .addOnFailureListener { e -> Log.w("Failed adding to DB", "Error writing document", e) }
+        val mF = MainFragment()
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.flFragment, mF)
+        transaction.disallowAddToBackStack()
+        transaction.commit()
 
 
     }
 
-    override fun onStart() {
+
+    /*
+    public override fun onStart() {
         super.onStart()
         // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser = mAuth!!.currentUser  // current user
+        val currentUser = auth.currentUser
     }
-
-    private fun createUser(email: String, password: String) {
-        mAuth!!.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        // Sign in success, update UI with the signed-in user's information
-                        Toast.makeText(
-                                this@MainActivity,
-                                "User successfully created!",
-                                Toast.LENGTH_SHORT
-                        ).show()
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Toast.makeText(this@MainActivity, "Creating user failed", Toast.LENGTH_SHORT).show()
-                    }
-                }
-    }
-
-    private fun login(email: String, password: String) {
-        mAuth!!.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(
-                        this
-                ) { task ->
-                    if (task.isSuccessful) {
-                        // Sign in success, update UI with the signed-in user's information
-                        Toast.makeText(this@MainActivity, "Logged in!", Toast.LENGTH_SHORT).show()
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Toast.makeText(this@MainActivity, "Login failed", Toast.LENGTH_SHORT).show()
-                    }
-                }
-    }
-
-
+     */
 
 
 
