862e84acff374af7cb354fb8e193082ac37d0884
jesperu7 <jesperu7@hotmail.com>
2021-01-27 12:29:36 +0100

    cleaned up more code

--- Alpha/app/src/main/java/com/cn/bsc/MainActivity.kt
+++ Alpha/app/src/main/java/com/cn/bsc/MainActivity.kt
@@ -1,25 +1,12 @@
 package com.cn.bsc
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.cn.bsc.fragments.LoginFragment
 import com.cn.bsc.fragments.MainFragment
-import com.cn.bsc.fragments.RegisterFragment
-import com.google.android.gms.tasks.OnFailureListener
-import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.firestore.DocumentReference
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.ktx.Firebase
-import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-
 
     private lateinit var auth: FirebaseAuth
 
@@ -37,7 +24,6 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-
     /*
     public override fun onStart() {
         super.onStart()
@@ -45,9 +31,5 @@ class MainActivity : AppCompatActivity() {
         val currentUser = auth.currentUser
     }
      */
-
-
-
-
 
 }
