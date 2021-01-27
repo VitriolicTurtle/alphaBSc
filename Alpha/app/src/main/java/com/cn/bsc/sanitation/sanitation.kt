b1faf827d20f7a38a9e96b3311247c83f1d49c5c
andrbl <andreasblakli@gmail.com>
2021-01-27 01:51:26 +0100

    Added external library EasyValidation for user input sanitation

---
+++ Alpha/app/src/main/java/com/cn/bsc/sanitation/sanitation.kt
@@ -0,0 +1,12 @@
+package com.cn.bsc.sanitation
+
+import android.widget.Toast
+import com.wajahatkarim3.easyvalidation.core.view_ktx.validEmail
+
+class Sanitation {
+    var emailStr = "john.doe@gmail.com"
+    var isValid = emailStr.validEmail()
+
+    
+
+}
