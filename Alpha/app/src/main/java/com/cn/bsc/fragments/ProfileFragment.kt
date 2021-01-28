bd4f67c497ab607c9ae6ef83d08dc0589991a3b1
Vegardoa <voaarnes@gmail.com>
2021-01-28 14:30:13 +0100

    Recycler view list for classrooms added. Profile fragment added and linked to menu.

---
+++ Alpha/app/src/main/java/com/cn/bsc/fragments/ProfileFragment.kt
@@ -0,0 +1,24 @@
+package com.cn.bsc.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.cn.bsc.R
+import com.cn.bsc.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+
+    private lateinit var binding: FragmentProfileBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_profile, container, false)
+
+
+
+        return binding.root
+    }
+
+}
