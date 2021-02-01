9b026e54aabbb8e1265bdb48ae2d48c47fc71a4c
jesperu7 <jesperu7@hotmail.com>
2021-02-01 13:48:00 +0100

    changed register fragment, can now register as either student or teacher

--- Alpha/app/src/main/java/com/cn/bsc/fragments/RegisterFragment.kt
+++ Alpha/app/src/main/java/com/cn/bsc/fragments/RegisterFragment.kt
@@ -7,9 +7,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.cn.bsc.R
 import com.cn.bsc.databinding.FragmentRegisterBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -33,24 +35,30 @@ class RegisterFragment : Fragment() {
         // getting data from the text fields
         val textFieldUsername = binding.root.findViewById<EditText>(R.id.et_register_email)
         val textFieldPassword = binding.root.findViewById<EditText>(R.id.et_register_password)
+        val textFieldName = binding.root.findViewById<EditText>(R.id.et_register_name)
+        val checkBox = binding.root.findViewById<CheckBox>(R.id.cb_isTeacher)
         val buttonRegister = binding.root.findViewById<Button>(R.id.btn_register)
         buttonRegister.setOnClickListener(){
             val userEmail = textFieldUsername.text.toString()
             val password = textFieldPassword.text.toString()
+            val name = textFieldName.text.toString()
+            val isTeacher = checkBox.isChecked
             // logging in using the data entered in the text fields
-            createUser(userEmail, password)
+            createUser(userEmail, password, name, isTeacher)
             auth.signOut()
         }
         return binding.root
    }     
 
-    private fun createUser(email: String, password: String) {
+    private fun createUser(email: String, password: String, name: String, isTeacher: Boolean) {
+
         auth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener() { task ->
                     if (task.isSuccessful) {
                         // Sign in success
-                        createDatabaseEntry(email)
+                        createDatabaseEntry(email, name, isTeacher)
                         Toast.makeText(activity,"User successfully created!",Toast.LENGTH_SHORT).show()
+                        findNavController().navigate(R.id.dest_start)
                     } else {
                         // If sign in fails, display a message to the user.
                         Toast.makeText(activity,"Error registering user!",Toast.LENGTH_SHORT).show()
@@ -59,16 +67,16 @@ class RegisterFragment : Fragment() {
                 }
     }
 
-    private fun createDatabaseEntry(email: String) {
+    private fun createDatabaseEntry(email: String, name: String, isTeacher: Boolean) {
 
         val userID = FirebaseAuth.getInstance().currentUser!!.uid
-        val userName = "testing"
         // Create a new user entry in the database
         val user = hashMapOf(
                 "userid" to userID,
                 "email" to email,
-                "name" to userName,
-                "score" to 0
+                "name" to name,
+                "score" to 0,
+                "teacher" to isTeacher
         )
 
         // add selected data to database
